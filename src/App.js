bc00b3bb57e48abb8cef2e8439349c0e8de69e99
Mintchoco12 <michaelzhang5654@gmail.com>
2023-01-24 21:12:50 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import Nav from './Nav';
+import Banner from './Banner';
 import Row from './Row';
 import request from './request';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals}/>
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} isLargeRow={true}/>
       <Row title="Trending Now" fetchUrl={request.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={request.fetchDocumantaries}/>
     </div>
   );
 }
